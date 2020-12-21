d2efafdf2deca6ae0b205ff7f0fe253982b430e5
Juggernaut9434 <juggernaut9434@gmail.com>
2020-12-21 13:38:19 -0700

    StockScrubber clean up Part 2 some more documentation and fixing typos

--- stockScrub/StockScrubber.sh
+++ stockScrub/StockScrubber.sh
@@ -38,17 +38,23 @@ done
 # https://www.grymoire.com/Unix/Sed.html#uh-10a
 
 # More clean up for single line
+#
 # tr is for characters and sed is for string matches
-cat results.txt | tr -d [a-zA-Z\<\/\>\"=] | tr '\n' ',' | tr -d [:space:] | sed -e 's/,,,/,/' -e 's/,,/,/g' -e 's/,,/,\n,/g' > results.txt
+# sed allows multiple expressions that substitutes
+# the `g` allows for all matches for that line otherwise first occurence
+#
+cat results.txt | tr -d [a-zA-Z\<\/\>\"=] | tr '\n' ',' | tr -d [:space:] |
+sed -e 's/,,,/,/' -e 's/,,/,/g' -e 's/,,/,\n,/g' > results.txt
 
 # put ticker in front of the line
-echo "" > temp.txt
+echo "" > temp.txt  # clean temp file with empty first line
 c=1
 for i in ${TICKERS[@]}
 do
     cat results.txt | sed "$c s/^/$i/" | sed -n "${c}p" >> temp.txt
     ((c=c+1))   # increment counter
 done
+
 # put header on the csv
 cat temp.txt | sed "1s/^/Ticker,Yield,EPS,PE,/" > results.csv
 # clean up other files
